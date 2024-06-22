3c027ec1b2b2c832a10cb2e2bae536023c224ee6
bbaktaeho <bbaktaeho@gmail.com>
2024-06-22 20:27:09 +0900

    fix: ethereum jsonrpc procedure

--- procedure.go
+++ procedure.go
@@ -1,57 +1,75 @@
 package evmc
 
-type Procedure string
+import (
+	"errors"
+	"reflect"
 
-const (
-	Web3ClientVersion Procedure = "web3_clientVersion"
-
-	EthCall                          Procedure = "eth_call"
-	EthChainID                       Procedure = "eth_chainId"
-	EthGetCode                       Procedure = "eth_getCode"
-	EthBlockNumber                   Procedure = "eth_blockNumber"
-	EthGetBlockByNumber              Procedure = "eth_getBlockByNumber"
-	EthGetBlockByHash                Procedure = "eth_getBlockByHash"
-	EthGetUncleByBlockNumberAndIndex Procedure = "eth_getUncleByBlockNumberAndIndex"
-	EthGetTransaction                Procedure = "eth_getTransactionByHash"
-	EthGetReceipt                    Procedure = "eth_getTransactionReceipt"
-	EthGetBalance                    Procedure = "eth_getBalance"
-	EthGetStorageAt                  Procedure = "eth_getStorageAt"
-	EthGetBlockReceipts              Procedure = "eth_getBlockReceipts"
-	EthGetLogs                       Procedure = "eth_getLogs"
-
-	EthGetTransactionReceiptsByBlock Procedure = "eth_getTransactionReceiptsByBlock" // bor
-
-	DebugTraceBlockByNumber Procedure = "debug_traceBlockByNumber"
-	DebugTraceTransaction   Procedure = "debug_traceTransaction"
-
-	OtsGetContractCreator Procedure = "ots_getContractCreator" // erigon
-	TraceBlock            Procedure = "trace_block"            // erigon
-
-	// arb_trace methods on the Arbitrum One chain should be called on blocks prior to 22207815
-	ArbitraceBlock Procedure = "arbtrace_block" // arbitrum
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
-func (p Procedure) String() string {
+type procedure string
+
+const (
+	web3ClientVersion procedure = "web3_clientVersion"
+
+	ethCall                          procedure = "eth_call"
+	ethChainID                       procedure = "eth_chainId"
+	ethGetCode                       procedure = "eth_getCode"
+	ethBlockNumber                   procedure = "eth_blockNumber"
+	ethGetBlockByNumber              procedure = "eth_getBlockByNumber"
+	ethGetBlockByHash                procedure = "eth_getBlockByHash"
+	ethGetUncleByBlockNumberAndIndex procedure = "eth_getUncleByBlockNumberAndIndex"
+	ethGetTransaction                procedure = "eth_getTransactionByHash"
+	ethGetReceipt                    procedure = "eth_getTransactionReceipt"
+	ethGetBalance                    procedure = "eth_getBalance"
+	ethGetStorageAt                  procedure = "eth_getStorageAt"
+	ethGetBlockReceipts              procedure = "eth_getBlockReceipts"
+	ethGetLogs                       procedure = "eth_getLogs"
+	ethGetTransactionReceiptsByBlock procedure = "eth_getTransactionReceiptsByBlock" // bor
+
+	debugTraceBlockByNumber procedure = "debug_traceBlockByNumber"
+	debugTraceTransaction   procedure = "debug_traceTransaction"
+
+	otsGetContractCreator procedure = "ots_getContractCreator" // erigon
+	traceBlock            procedure = "trace_block"            // erigon
+
+	// arb_trace methods on the Arbitrum One chain should be called on blocks prior to 22207815
+	arbitraceBlock procedure = "arbtrace_block" // arbitrum
+)
+
+func (p procedure) String() string {
 	return string(p)
 }
 
-type BlockTag string
+type blockTag string
 
 const (
-	Latest    BlockTag = "latest"
-	Safe      BlockTag = "safe"
-	Finalized BlockTag = "finalized"
+	Latest    blockTag = "latest"
+	Safe      blockTag = "safe"
+	Finalized blockTag = "finalized"
 )
 
-func (bt BlockTag) String() string {
-	switch bt {
-	case Latest:
-		return "latest"
-	case Safe:
-		return "safe"
-	case Finalized:
-		return "finalized"
+func (bt blockTag) String() string {
+	return string(bt)
+}
+
+// TODO: error message
+func parseNumOrTag(numOrTag interface{}) (string, error) {
+	if numOrTag == nil {
+		return Latest.String(), nil
+	}
+	switch reflect.TypeOf(numOrTag).Kind() {
+	case reflect.Int:
+		return hexutil.EncodeUint64(uint64(numOrTag.(int))), nil
+	case reflect.Uint, reflect.Uint64:
+		return hexutil.EncodeUint64((numOrTag.(uint64))), nil
+	case reflect.String:
+		tag, ok := numOrTag.(blockTag)
+		if !ok {
+			return "", errors.New("invalid block tag type")
+		}
+		return tag.String(), nil
 	default:
-		return ""
+		return "", errors.New("invalid block tag type")
 	}
 }
