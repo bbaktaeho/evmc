f9db462a598b3c74d94cf51150eef6e416c4fe25
bbaktaeho <bbaktaeho@gmail.com>
2024-06-22 14:02:34 +0900

    add: basic example

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "github.com/bbaktaeho/evmc"
 
+func main() {
+	// set url to connect to blockchain node
+	client, err := evmc.New("https://ethereum-mainnet.nodit.io")
+	if err != nil {
+		panic(err)
+	}
+
+	chainID := client.ChainID()
+	println(chainID)
+	nodeName, nodeVersion := client.NodeClient()
+	println(nodeName, nodeVersion)
+
+	chainID, err = client.Eth().GetChainID()
+	if err != nil {
+		panic(err)
+	}
+	println(chainID)
+
+	cv, err := client.Web3().ClientVersion()
+	if err != nil {
+		panic(err)
+	}
+	println(cv)
 }
