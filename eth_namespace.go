384f54e8e3403249bcc3b52b621df49ae1041f3a
bbaktaeho <bbaktaeho@gmail.com>
2024-06-23 18:01:48 +0900

    fix: func name

--- eth_namespace.go
+++ eth_namespace.go
@@ -10,9 +10,17 @@ type ethNamespace struct {
 	c caller
 }
 
-func (e *ethNamespace) GetChainID() (uint64, error) {
+func (e *ethNamespace) ChainID() (uint64, error) {
+	return e.chainID(context.Background())
+}
+
+func (e *ethNamespace) ChainIDWithContext(ctx context.Context) (uint64, error) {
+	return e.chainID(ctx)
+}
+
+func (e *ethNamespace) chainID(ctx context.Context) (uint64, error) {
 	result := new(string)
-	if err := e.c.call(context.Background(), result, ethChainID); err != nil {
+	if err := e.c.call(ctx, result, ethChainID); err != nil {
 		return 0, err
 	}
 	id, err := hexutil.DecodeUint64(*result)
