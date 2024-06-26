bc21b792525fca0c0a9363247d3ca1124af46671
bbaktaeho <bbaktaeho@gmail.com>
2024-06-26 21:44:39 +0900

    fix: base example

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/bbaktaeho/evmc"
+import (
+	"fmt"
+
+	"github.com/bbaktaeho/evmc"
+)
 
 func main() {
 	// set url to connect to blockchain node
@@ -10,65 +14,95 @@ func main() {
 	}
 
 	chainID := client.ChainID()
-	println(chainID)
+	fmt.Println(chainID)
 	nodeName, nodeVersion := client.NodeClient()
-	println(nodeName, nodeVersion)
+	fmt.Println(nodeName, nodeVersion)
 
 	chainID, err = client.Eth().ChainID()
 	if err != nil {
 		panic(err)
 	}
-	println(chainID)
+	fmt.Println(chainID)
 
 	cv, err := client.Web3().ClientVersion()
 	if err != nil {
 		panic(err)
 	}
-	println(cv)
+	fmt.Println(cv)
 
 	storage, err := client.Eth().GetStorageAt("0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", "0x0", nil)
 	if err != nil {
 		panic(err)
 	}
-	println(storage)
+	fmt.Println(storage)
 
 	latestBlockNumber, err := client.Eth().GetBlockNumber()
 	if err != nil {
 		panic(err)
 	}
-	println(latestBlockNumber)
+	fmt.Println(latestBlockNumber)
 
 	bytecode, err := client.Eth().GetCode("0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", nil)
 	if err != nil {
 		panic(err)
 	}
-	println(bytecode)
+	fmt.Println(bytecode)
 
 	block, err := client.Eth().GetBlockByNumber(latestBlockNumber)
 	if err != nil {
 		panic(err)
 	}
-	println(block.Number, block.Hash, len(block.Transactions))
+	fmt.Println(block.Number, block.Hash, len(block.Transactions))
 
 	blockIncTx, err := client.Eth().GetBlockByNumberIncTx(latestBlockNumber)
 	if err != nil {
 		panic(err)
 	}
 	for _, tx := range blockIncTx.Transactions {
-		println(tx.Hash, tx.From, tx.To, tx.Value)
+		fmt.Println(tx.Hash, tx.From, tx.To, tx.Value)
 	}
 
 	block, err = client.Eth().GetBlockByHash(block.Hash)
 	if err != nil {
 		panic(err)
 	}
-	println(block.Number, block.Hash, len(block.Transactions))
+	fmt.Println(block.Number, block.Hash, len(block.Transactions))
 
 	blockIncTx, err = client.Eth().GetBlockByHashIncTx(block.Hash)
 	if err != nil {
 		panic(err)
 	}
 	for _, tx := range blockIncTx.Transactions {
-		println(tx.Hash, tx.From, tx.To, tx.Value)
+		fmt.Println(tx.Hash, tx.From, tx.To, tx.Value)
 	}
+
+	logs, err := client.Eth().GetLogsByBlockNumber(20141602)
+	if err != nil {
+		panic(err)
+	}
+	for _, log := range logs {
+		fmt.Println(log.Address, log.BlockHash, log.Data, log.LogIndex, len(log.Topics))
+	}
+
+	logs, err = client.Eth().GetLogsByBlockHash("0x5a5310472efc52cc3a7afa2460be8438e88d32d7d59fb06da6bb887a146a2001")
+	if err != nil {
+		panic(err)
+	}
+	for _, log := range logs {
+		fmt.Println(log.Address, log.BlockHash, log.Data, log.LogIndex, len(log.Topics))
+	}
+
+	receipts, err := client.Eth().GetBlockReceipts(20141602)
+	if err != nil {
+		panic(err)
+	}
+	for _, receipt := range receipts {
+		fmt.Println(receipt.BlockHash, receipt.TransactionHash, receipt.Status, receipt.CumulativeGasUsed)
+	}
+
+	gasPrice, err := client.Eth().GasPrice()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(gasPrice)
 }
