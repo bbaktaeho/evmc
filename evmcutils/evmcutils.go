0a788cf148f34fc8b9906209211b7aac96433a5f
bbaktaeho <bbaktaeho@gmail.com>
2024-07-03 23:59:01 +0900

    add: evmcutils package

--- evmcutils/evmcutils.go
+++ evmcutils/evmcutils.go
@@ -1,43 +1,41 @@
 package evmcutils
 
 import (
-	"github.com/bbaktaeho/evmc"
-	"github.com/ethereum/go-ethereum/accounts/abi"
+	"errors"
+	"regexp"
+
+	"github.com/bbaktaeho/evmc/evmcsoltypes"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/common/lru"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-type method struct {
-	selector *abi.SelectorMarshaling
-	id       string
-}
-
+// TODO: geth abi.ABI
 const (
 	defaultMethodCacheCapacity = 100
 )
 
 var (
-	methodCache *lru.Cache[string, *method]
+	methodCache   *lru.Cache[string, string]
+	methodPattern = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*\((\w+(\[\d*\])?(,\w+(\[\d*\])?)*)?\)$`)
 )
 
 func init() {
-	methodCache = lru.NewCache[string, *method](defaultMethodCacheCapacity)
+	methodCache = lru.NewCache[string, string](defaultMethodCacheCapacity)
 }
 
-func GenerateTxInput(funcSig string, args ...evmc.SolType) (string, error) {
-	m, ok := methodCache.Get(funcSig)
+func GenerateTxInput(funcSig string, args ...evmcsoltypes.SolType) (string, error) {
+	mid, ok := methodCache.Get(funcSig)
 	if !ok {
-		selector, err := abi.ParseSelector(funcSig)
-		if err != nil {
-			return "", err
+		if !methodPattern.MatchString(funcSig) {
+			return "", errors.New("invalid function signature")
 		}
 		keccak := crypto.Keccak256([]byte(funcSig))
 		id := hexutil.Encode(keccak[:])[0:10]
-		m = &method{selector: &selector, id: id}
-		methodCache.Add(funcSig, m)
+		mid = id
+		methodCache.Add(funcSig, id)
 	}
-	idBytes := hexutil.MustDecode(m.id)
+	idBytes := hexutil.MustDecode(mid)
 	input := make([]byte, len(idBytes))
 	copy(input, idBytes)
 	for _, arg := range args {
