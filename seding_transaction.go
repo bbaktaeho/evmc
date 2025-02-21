428061f67d6d4a32aee22d59dac5969004811218
bbaktaeho <bbaktaeho@gmail.com>
2025-02-21 10:21:31 +0900

    add: setCode tx and refactoring

--- seding_transaction.go
+++ seding_transaction.go
@@ -2,11 +2,18 @@ package evmc
 
 import (
 	"errors"
+	"fmt"
+	"math/big"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/holiman/uint256"
 	"github.com/shopspring/decimal"
+	"golang.org/x/crypto/sha3"
 )
 
 // Tx is a structure that contains transaction information
@@ -19,6 +26,8 @@ type Tx struct {
 	Value    decimal.Decimal `json:"value"`
 	GasPrice decimal.Decimal `json:"gasPrice"` // legacy
 	GasLimit uint64          `json:"gasLimit"`
+
+	ChainID uint64 `json:"chainId"` // EIP-155
 
 	AccessList []struct {
 		Address     string   `json:"address"`
@@ -54,7 +63,10 @@ func (t *Tx) parseCallMsg() (map[string]interface{}, error) {
 	}, nil
 }
 
-func (t *Tx) checkSendingTx() error {
+func (t *Tx) valid() error {
+	if t.ChainID < 0 {
+		return ErrChainIDLessThanZero
+	}
 	if t.To == "" {
 		return ErrToRequired
 	}
@@ -62,13 +74,13 @@ func (t *Tx) checkSendingTx() error {
 		return ErrTxGasLimitZero
 	}
 	if t.GasPrice.IsNegative() {
-		return ErrTxGasPriceZero
+		return ErrTxGasPriceLessThanZero
 	}
 	if t.MaxFeePerGas.IsNegative() {
-		return ErrTxMaxFeePerGasZero
+		return ErrTxMaxFeePerGasLessThanZero
 	}
 	if t.MaxPriorityFeePerGas.IsNegative() {
-		return ErrTxMaxPriorityFeePerGasZero
+		return ErrTxMaxPriorityFeePerGasLessThanZero
 	}
 	if t.Value.IsNegative() {
 		return ErrTxValueLessThanZero
@@ -81,9 +93,6 @@ type SendingTx struct {
 }
 
 func NewSendingTx(tx *Tx) (*SendingTx, error) {
-	if err := tx.checkSendingTx(); err != nil {
-		return nil, err
-	}
 	if !tx.GasPrice.IsZero() {
 		return NewLegacyTx(tx)
 	}
@@ -91,12 +100,12 @@ func NewSendingTx(tx *Tx) (*SendingTx, error) {
 }
 
 func NewLegacyTx(tx *Tx) (*SendingTx, error) {
+	if err := tx.valid(); err != nil {
+		return nil, err
+	}
 	data, toAddress, err := parseDataAndToAddress(tx.Data, tx.To)
 	if err != nil {
 		return nil, err
-	}
-	if tx.GasPrice.IsZero() {
-		return nil, errors.New("gas price is zero")
 	}
 	legacyTx := &types.LegacyTx{
 		Nonce:    tx.Nonce,
@@ -110,12 +119,12 @@ func NewLegacyTx(tx *Tx) (*SendingTx, error) {
 }
 
 func NewAccessListTx(tx *Tx) (*SendingTx, error) {
+	if err := tx.valid(); err != nil {
+		return nil, err
+	}
 	data, toAddress, err := parseDataAndToAddress(tx.Data, tx.To)
 	if err != nil {
 		return nil, err
-	}
-	if tx.GasPrice.IsZero() {
-		return nil, errors.New("gas price is zero")
 	}
 	accessList := make([]types.AccessTuple, len(tx.AccessList))
 	for i, access := range tx.AccessList {
@@ -129,6 +138,7 @@ func NewAccessListTx(tx *Tx) (*SendingTx, error) {
 		}
 	}
 	accessListTx := &types.AccessListTx{
+		ChainID:    decimal.NewFromUint64(tx.ChainID).BigInt(),
 		Nonce:      tx.Nonce,
 		To:         &toAddress,
 		Value:      tx.Value.BigInt(),
@@ -142,14 +152,15 @@ func NewAccessListTx(tx *Tx) (*SendingTx, error) {
 
 // TODO: auto setting maxPriorityFeePerGas and maxFeePerGas
 func NewDynamicFeeTx(tx *Tx) (*SendingTx, error) {
+	if err := tx.valid(); err != nil {
+		return nil, err
+	}
 	data, toAddress, err := parseDataAndToAddress(tx.Data, tx.To)
 	if err != nil {
 		return nil, err
 	}
-	if tx.MaxFeePerGas.IsZero() {
-		return nil, errors.New("maxFeePerGas is zero")
-	}
 	dynamicFeeTx := &types.DynamicFeeTx{
+		ChainID:   decimal.NewFromUint64(tx.ChainID).BigInt(),
 		Nonce:     tx.Nonce,
 		To:        &toAddress,
 		Value:     tx.Value.BigInt(),
@@ -159,6 +170,97 @@ func NewDynamicFeeTx(tx *Tx) (*SendingTx, error) {
 		Data:      data,
 	}
 	return &SendingTx{txData: dynamicFeeTx}, nil
+}
+
+type SetCodeAuthorization struct {
+	ChainID uint64
+	Address string
+	Nonce   uint64
+}
+
+type SignedSetCodeAuthorization struct {
+	SetCodeAuthorization
+	V uint8
+	R *big.Int
+	S *big.Int
+}
+
+var hasherPool = sync.Pool{
+	New: func() any { return sha3.NewLegacyKeccak256() },
+}
+
+func decodeSignature(sig []byte) (r, s, v *big.Int) {
+	if len(sig) != crypto.SignatureLength {
+		panic(fmt.Sprintf("wrong size for signature: got %d, want %d", len(sig), crypto.SignatureLength))
+	}
+	r = new(big.Int).SetBytes(sig[:32])
+	s = new(big.Int).SetBytes(sig[32:64])
+	v = new(big.Int).SetBytes([]byte{sig[64] + 27})
+	return r, s, v
+}
+
+func SignSetCode(wallet *Wallet, auth SetCodeAuthorization) (*SignedSetCodeAuthorization, error) {
+	if !common.IsHexAddress(auth.Address) {
+		return nil, errors.New("invalid SetCodeAuthorization address")
+	}
+	var (
+		sha = hasherPool.Get().(crypto.KeccakState)
+		h   common.Hash
+	)
+	defer hasherPool.Put(sha)
+
+	sha.Reset()
+	sha.Write([]byte{0x05})
+	rlp.Encode(sha, []any{*uint256.NewInt(auth.ChainID), common.HexToAddress(auth.Address), auth.Nonce})
+	sha.Read(h[:])
+
+	sig, err := crypto.Sign(h[:], wallet.pk)
+	if err != nil {
+		return nil, err
+	}
+	r, s, _ := decodeSignature(sig)
+	return &SignedSetCodeAuthorization{
+		SetCodeAuthorization: auth,
+		V:                    sig[64],
+		R:                    r,
+		S:                    s,
+	}, nil
+}
+
+func NewSetCodeTx(tx *Tx, signedAuthList []SignedSetCodeAuthorization) (*SendingTx, error) {
+	if err := tx.valid(); err != nil {
+		return nil, err
+	}
+	data, toAddress, err := parseDataAndToAddress(tx.Data, tx.To)
+	if err != nil {
+		return nil, err
+	}
+	authorizations := make([]types.SetCodeAuthorization, len(signedAuthList))
+	for i, auth := range signedAuthList {
+		if !common.IsHexAddress(auth.Address) {
+			return nil, errors.New("invalid SetCodeAuthorization address")
+		}
+		authorizations[i] = types.SetCodeAuthorization{
+			ChainID: *uint256.NewInt(auth.ChainID),
+			Address: common.HexToAddress(auth.Address),
+			Nonce:   auth.Nonce,
+			V:       auth.V,
+			R:       *uint256.MustFromBig(auth.R),
+			S:       *uint256.MustFromBig(auth.S),
+		}
+	}
+	setCodeTx := &types.SetCodeTx{
+		ChainID:   uint256.NewInt(tx.ChainID),
+		Nonce:     tx.Nonce,
+		To:        toAddress,
+		Value:     uint256.MustFromBig(tx.Value.BigInt()),
+		Gas:       tx.GasLimit,
+		GasFeeCap: uint256.MustFromBig(tx.MaxFeePerGas.BigInt()),
+		Data:      data,
+		GasTipCap: uint256.MustFromBig(tx.MaxPriorityFeePerGas.BigInt()),
+		AuthList:  authorizations,
+	}
+	return &SendingTx{txData: setCodeTx}, nil
 }
 
 func parseDataAndToAddress(data string, to string) ([]byte, common.Address, error) {
